0e176cd58f1d87e0786a245d164e3d493609abba
Krste11 <angelkrstevski66@gmail.com>
2025-02-26 20:36:08 +0100

    Add files via upload

---
+++ 4_C# Basic Homeworks/1_homework/1_homework/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _1_homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First Homework...");
+        }
+    }
+}
