3f76df08e80af01effb7cbdeed24ba5e2af402f9
Krste11 <angelkrstevski66@gmail.com>
2025-03-02 18:30:05 +0100

    Add files via upload

---
+++ 4_C# Basic Homeworks/1_homework/2_Exercise/2_Exercise/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace _2_Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter second number ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int largerNumber = default;
+
+            if(num1 > num2)
+            {
+                largerNumber = num1;
+                Console.WriteLine(num1 + " is larger");
+            }
+            else if(num2 > num1)
+            {
+                largerNumber = num2;
+                Console.WriteLine(num2 + " is larger");
+            }
+            else
+            {
+                largerNumber = num1;
+                Console.WriteLine("They are equal");
+            }
+
+            if(largerNumber == 0)
+            {
+                Console.WriteLine("The number is 0");
+            }
+            else if(largerNumber % 2 == 0)
+            {
+                Console.WriteLine("Number " + largerNumber + " is odd");
+            }
+            else
+            {
+                Console.WriteLine("Number " + largerNumber + " is even");
+            }
+        }
+    }
+}
