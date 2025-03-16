1bac698ba652148f77c76e53d75946840bb2d62e
Krste11 <angelkrstevski66@gmail.com>
2025-03-16 18:13:20 +0100

    Add files via upload

---
+++ Class05-Workshop/jedi-archives/src/app.js
@@ -0,0 +1,20 @@
+// 1) DECLARE/INITIALIZE GLOBAL VARIABLES AND CONSTANTS
+const BASE_API_URL = "https://swapi.dev/api/";
+
+// 2) SELECTING ELEMENTS
+const peopleBtn = document.getElementById("peopleBtn");
+const shipsBtn = document.getElementById("shipsBtn");
+const loader = document.getElementById("loader");
+const resultDiv = document.getElementById("result");
+const prevBtn = document.getElementById("prevBtn");
+const nextBtn = document.getElementById("nextBtn");
+
+
+// 3) FUNCTION DEFINITIONS
+
+
+
+// 4) HANDLING EVENTS
+
+
+
