963cd38120f99182dacde69d722d0c439c3a847e
Krste11 <angelkrstevski66@gmail.com>
2025-04-30 12:43:03 +0200

    Add files via upload

---
+++ 6_C# Advanced Homeworks/1_homework/1_homework/1_homework/Interfaces/ISearchable.cs
@@ -0,0 +1,7 @@
+﻿namespace _1_homework.Interfaces
+{
+    public interface Searchable
+    {
+        bool Search(string word);
+    }
+}
---
+++ 6_C# Advanced Homeworks/1_homework/2_homework/2_homework/Interfaces/IShape.cs
@@ -0,0 +1,7 @@
+﻿namespace _2_homework.Interfaces
+{
+    public interface Shape
+    {
+        double GetArea();
+    }
+}
