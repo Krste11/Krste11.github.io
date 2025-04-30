6b54ceaa73cdacf5d1e44e87fc2ee9a9135b9fa9
Krste11 <angelkrstevski66@gmail.com>
2025-04-30 12:44:16 +0200

    Add files via upload

---
+++ 6_C# Advanced Homeworks/1_homework/1_homework/3_homework/3_homework/Entities/Shape.cs
@@ -0,0 +1,8 @@
+﻿namespace _3_homework.Entities
+{
+    public abstract class Shape
+    {
+        public abstract double CalculateArea();
+        public abstract double CalculatePerimeter();
+    }
+}
---
+++ 6_C# Advanced Homeworks/1_homework/1_homework/4_homework/4_homework/Program.cs
@@ -0,0 +1,12 @@
+﻿using _4_homework.Entitis;
+
+Manager manager = new Manager("Robert Panovski", 100, 5000m, 1500m);
+Programmer programmer = new Programmer("Bob Bobsky", 150, 50m, 160);
+
+Console.WriteLine("=== Manager Details ===");
+manager.DisplayInfo();
+manager.CalculateSalary();
+
+Console.WriteLine("\n=== Programmer Details ===");
+programmer.DisplayInfo();
+programmer.CalculateSalary();
