e123b1d77f8ed4af35c8cf9b945b75dc67affb08
Krste11 <angelkrstevski66@gmail.com>
2025-04-30 12:41:40 +0200

    Delete 6_C# Advanced Homeworks/1_homework directory

--- 6_C# Advanced Homeworks/1_homework/3_homework/3_homework/Entities/Shape.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace _3_homework.Entities
-{
-    public abstract class Shape
-    {
-        public abstract double CalculateArea();
-        public abstract double CalculatePerimeter();
-    }
-}
