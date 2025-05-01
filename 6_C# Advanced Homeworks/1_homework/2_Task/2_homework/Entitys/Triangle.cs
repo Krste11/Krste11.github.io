cbda5878ca43e0eeb0dfe9dfe1eebf65d5b82f95
Krste11 <angelkrstevski66@gmail.com>
2025-05-01 20:03:42 +0200

    Add files via upload

---
+++ 6_C# Advanced Homeworks/1_homework/2_Task/2_homework/Entitys/Triangle.cs
@@ -0,0 +1,21 @@
+﻿using _2_homework.Interfaces;
+
+namespace _2_homework.Entitys
+{
+    class Triangle : Shape
+    {
+        public double BaseLength { get; set; }
+        public double Height { get; set; }
+
+        public Triangle(double baseLength, double height)
+        {
+            BaseLength = baseLength;
+            Height = height;
+        }
+
+        public double GetArea()
+        {
+            return 0.5 * BaseLength * Height;
+        }
+    }
+}
---
+++ 6_C# Advanced Homeworks/1_homework/3_Task/3_homework/Entities/Circle.cs
@@ -0,0 +1,22 @@
+﻿namespace _3_homework.Entities
+{
+    public class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public override double CalculateArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * Math.PI * Radius;
+        }
+    }
+}
