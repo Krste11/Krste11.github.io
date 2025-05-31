750831cd1b50ddd3863f8c61b82f1568ce2d80ff
Krste11 <angelkrstevski66@gmail.com>
2025-05-31 10:54:35 +0200

    Add files via upload

---
+++ 12/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using ConsoleApp1.Servicis;
+
+string connectionString = "Server=.;Database=SEDC;Trusted_Connection=True;TrustServerCertificate=True;";
+
+StudentService studentService = new StudentService(connectionString);
+
+Console.WriteLine("Press enter if you want to fetch all students from the db");
+Console.ReadLine();
+
+var students = studentService.GetAllStudents();
+
+//students.ForEach(
+//    student =>  Console.WriteLine($"Student{student.FirstName} {student.LastName} | {student.Gender} | {student.NationalIdNumber} | {student.StudentCardNumber}"
+//    ));
+
+students.ForEach(student =>
+{
+    Console.WriteLine($"{student.Id,5} {student.FirstName,-15} {student.LastName,-15} {student.Gender,7} " +
+                      $"{(student.DateOfBirth?.ToString("yyyy-MM-dd") ?? "N/A"),12} " +
+                      $"{(student.EnrolledDate?.ToString("yyyy-MM-dd") ?? "N/A"),12} " +
+                      $"{(student.NationalIdNumber?.ToString() ?? "N/A"),10} " +
+                      $"{student.StudentCardNumber,-10}");
+});
+
+Console.WriteLine("Enter Id to fetch student by id: ");
+string studentId = Console.ReadLine();
+var studentById = studentService.GetStudentById(studentId);
+
+Console.WriteLine($"Student: {studentById.FirstName} {studentById.LastName} {studentById.Gender}");
---
+++ 12/ConsoleApp1/ConsoleApp1/Servicis/StudentService.cs
@@ -0,0 +1,85 @@
+﻿using ConsoleApp1.Entitis;
+using Microsoft.Data.SqlClient;
+
+namespace ConsoleApp1.Servicis
+{
+    public class StudentService
+    {
+        private readonly string _conString;
+        public StudentService(string conString)
+        {
+            _conString = conString;
+        }
+        public List<Student> GetAllStudents()
+        {
+            var students = new List<Student>();
+
+            using (SqlConnection conection = new SqlConnection(_conString))
+            {
+                conection.Open();
+
+                string query = "SELECT ID,FirstName,LastName,DateOfBirth, EnrolledDate,Gender,NationalIdNumber, StudentCardNumber from Student";
+
+                using (SqlCommand command = new SqlCommand(query, conection))
+                using (SqlDataReader reader = command.ExecuteReader()) 
+                {
+                    while (reader.Read())
+                    {
+                        students.Add(new Student
+                        {
+                            Id = reader.GetInt32(0),
+                            FirstName = reader.IsDBNull(1) ? null : reader.GetString(1),
+                            LastName = reader.IsDBNull(2) ? null : reader.GetString(2),
+                            DateOfBirth = reader.IsDBNull(3) ? null : reader.GetDateTime(3),
+                            EnrolledDate = reader.IsDBNull(4) ? null : reader.GetDateTime(4),
+                            Gender = reader.IsDBNull(5) ? null : reader.GetString(5)[0],
+                            NationalIdNumber = reader.IsDBNull(6) ? null : reader.GetInt64(6),
+                            StudentCardNumber = reader.IsDBNull(7) ? null : reader.GetString(7)
+                        });
+                    }
+                }
+            }
+            return students;
+        }
+        public Student GetStudentById(string id)
+        {
+            Student? student = null;
+            using (SqlConnection connection = new SqlConnection(_conString))
+            {
+                connection.Open();
+
+                string query = $"SELECT * from Student WHERE ID = {id}";
+                using(SqlCommand command = new SqlCommand(query, connection))
+                {
+                    using(SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if(reader.Read())
+                        {
+                            student = new Student()
+                            {
+                                Id = reader.GetFieldValue<int>(0),
+                                FirstName = reader.IsDBNull(1) ? null : reader.GetFieldValue<string>(1),
+                                LastName = reader.IsDBNull(2) ? null : reader.GetFieldValue<string>(2),
+                                DateOfBirth = reader.IsDBNull(3) ? null : reader.GetFieldValue<DateTime>(3),
+                                EnrolledDate = reader.IsDBNull(4) ? null : reader.GetFieldValue<DateTime>(4),
+                                Gender = reader.IsDBNull(5) ? null : reader.GetFieldValue<string>(5)[0],
+                                NationalIdNumber = reader.IsDBNull(6) ? null : reader.GetFieldValue<long>(6),
+                                StudentCardNumber = reader.IsDBNull(7) ? null : reader.GetFieldValue<string>(7),
+                            };
+                        }
+                    }
+                }
+            }
+            return student;
+        }
+        public void InsertStudent(Student student)
+        {
+            using (SqlConnection connection = new SqlConnection(_conString))
+            {
+                connection.Open();
+                
+                string query = $"INSER INTO Student (@ID,@FirstName,@LastName,@DateOfBirth, @EnrolledDate,@Gender,@NationalIdNumber, StudentCardNumber) VALUES(ID,FirstName,LastName,DateOfBirth, EnrolledDate,Gender,NationalIdNumber, StudentCardNumber";
+            } 
+        }
+    }
+}
