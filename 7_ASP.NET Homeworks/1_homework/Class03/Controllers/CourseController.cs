869a2edc9558cfd559fa69133fbb5619cf067c6a
Angel Krstevski <angelkrstevski66@gmail.com>
2025-06-18 12:29:49 +0200

    Add files via upload

---
+++ 7_ASP.NET Homeworks/1_homework/Class03/Controllers/CourseController.cs
@@ -0,0 +1,38 @@
+﻿using Class03.Models.DtoModels;
+using Class03.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Class03.Controllers
+{
+    [Route("courses")]
+    public class CourseController : Controller
+    {
+        private CourseService _courseService;
+
+        public CourseController()
+        {
+            _courseService = new CourseService();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCourseById(int id)
+        {
+            CourseByIdDto course = _courseService.GetCourseById(id);
+
+            if (course == null)
+            {
+                return Content("Course not found");
+            }
+
+            return Json(course);
+        }
+
+        [HttpGet("all")]
+        public IActionResult GetAllCourses()
+        {
+            List<ListAllCoursesDto> courses = _courseService.GetAllCourses();
+
+            return Json(courses);
+        }
+    }
+}
