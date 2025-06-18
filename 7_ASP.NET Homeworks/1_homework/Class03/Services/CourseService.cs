35f5ac47e14c135e7d631766a5948ac2a3ab82b6
Angel Krstevski <angelkrstevski66@gmail.com>
2025-06-18 12:33:37 +0200

    Add files via upload

---
+++ 7_ASP.NET Homeworks/1_homework/Class03/Services/CourseService.cs
@@ -0,0 +1,37 @@
+﻿using Class03.Database;
+using Class03.Models.DtoModels;
+using Class03.Models.Entities;
+
+namespace Class03.Services
+{
+    public class CourseService
+    {
+        public CourseByIdDto GetCourseById(int id)
+        {
+            Course course = InMemoryDb.Courses.SingleOrDefault(x => x.Id == id);
+
+            if (course == null)
+            {
+                return null;
+            }
+
+            CourseByIdDto courseByIdDto = new CourseByIdDto
+            {
+                Id = course.Id,
+                Name = course.Name,
+                NumberOfClasses = course.NumberOfClasses,
+            };
+
+            return courseByIdDto;
+        }
+
+        public List<ListAllCoursesDto> GetAllCourses()
+        {
+            return InMemoryDb.Courses.Select(course =>
+            new ListAllCoursesDto
+            {
+                NameOfCourse = course.Name
+            }).ToList();
+        }
+    }
+}
