fa3c6075d2a031d8f42dadb47b7f74f62516da3d
Angel Krstevski <angelkrstevski66@gmail.com>
2025-06-18 12:32:23 +0200

    Add files via upload

---
+++ 7_ASP.NET Homeworks/1_homework/Class03/Models/DtoModels/CourseByIdDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Class03.Models.DtoModels
+{
+    public class CourseByIdDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int NumberOfClasses { get; set; }
+    }
+}
---
+++ 7_ASP.NET Homeworks/1_homework/Class03/Models/DtoModels/ListAllCoursesDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Class03.Models.DtoModels
+{
+    public class ListAllCoursesDto
+    {
+        public string NameOfCourse { get; set; }
+    }
+}
