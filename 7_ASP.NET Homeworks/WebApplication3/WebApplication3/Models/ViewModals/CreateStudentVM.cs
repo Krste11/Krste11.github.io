609464fc4db6767e72bfeaa066876f1d8a4a5750
Angel Krstevski <angelkrstevski66@gmail.com>
2025-06-23 20:03:19 +0200

    Add files via upload

---
+++ 7_ASP.NET Homeworks/WebApplication3/WebApplication3/Models/ViewModals/CreateStudentVM.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication3.Models.ViewModals
+{
+    public class CreateStudentVM
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int? ActiveCourseId { get; set; }
+        public List<CourseOptionVM> Courses { get; set; } 
+    }
+}
