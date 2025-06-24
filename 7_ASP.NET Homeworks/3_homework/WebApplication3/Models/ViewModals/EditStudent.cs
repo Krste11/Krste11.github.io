398e4a6b06b47702096bf57919c6aaae8435cdbc
Angel Krstevski <angelkrstevski66@gmail.com>
2025-06-24 11:40:21 +0200

    Add files via upload

---
+++ 7_ASP.NET Homeworks/3_homework/WebApplication3/Models/ViewModals/EditStudent.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication3.Models.ViewModals
+{
+    public class EditStudent
+    {
+        public int Id { get; set; }  
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int? ActiveCourseId { get; set; }
+        public List<CourseOptionVM> Courses { get; set; }
+    }
+}
