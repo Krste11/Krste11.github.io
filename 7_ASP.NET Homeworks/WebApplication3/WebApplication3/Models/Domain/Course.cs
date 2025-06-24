ce07b948236f3ab763b0bad6171ffc828b110ee0
Angel Krstevski <angelkrstevski66@gmail.com>
2025-06-24 11:35:31 +0200

    Delete 7_ASP.NET Homeworks/WebApplication3 directory

--- 7_ASP.NET Homeworks/WebApplication3/WebApplication3/Models/Domain/Course.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebApplication3.Models.Domain
-{
-    public class Course
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int NumberOfClasses { get; set; }
-    }
-}
--- 7_ASP.NET Homeworks/WebApplication3/WebApplication3/Models/ViewModals/CourseOptionVM.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WebApplication3.Models.ViewModals
-{
-    public class CourseOptionVM
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
-}
