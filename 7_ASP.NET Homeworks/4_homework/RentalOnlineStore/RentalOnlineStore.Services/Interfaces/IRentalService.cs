175ec5be1e74630cba6361cac2af5443c6f45849
Angel Krstevski <angelkrstevski66@gmail.com>
2025-07-26 17:49:42 +0200

    Add files via upload

---
+++ 7_ASP.NET Homeworks/4_homework/RentalOnlineStore/RentalOnlineStore.Services/Interfaces/IRentalService.cs
@@ -0,0 +1,10 @@
+﻿using RentalOnlineStore.Domain.Models;
+
+namespace RentalOnlineStore.Services.Interfaces
+{
+    public interface IRentalService
+    {
+        List<Rental> GetRentalsByUserId(int userId);
+        bool ReturnMovie(int rentalId);
+    }
+}
