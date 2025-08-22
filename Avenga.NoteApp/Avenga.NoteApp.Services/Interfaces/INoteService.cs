6a95186e4e54bfe8f153b9611c260e0a05c6c394
Angel Krstevski <angelkrstevski66@gmail.com>
2025-08-22 16:15:25 +0200

    Add files via upload

---
+++ Avenga.NoteApp/Avenga.NoteApp.Services/Interfaces/INoteService.cs
@@ -0,0 +1,13 @@
+﻿using Avenga.NoteApp.Dtos.NotesDto;
+
+namespace Avenga.NoteApp.Services.Interfaces
+{
+    public interface INoteService
+    {
+        List<NoteDto> GetAllNotes();
+        NoteDto GetById(int id);
+        void AddNote(AddNoteDto note);
+        void UpdateNote(UpdateNoteDto note);
+        void DeleteNote(int id);
+    }
+}
