72ae9a8b0ef34d10f77057b79743bb60c2c1dba8
Angel Krstevski <angelkrstevski66@gmail.com>
2025-08-26 14:30:21 +0200

    Add files via upload

---
+++ MovieAppSolution/MovieApp/MovieApp.Helpers/DependencyInjectionHelper.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MovieApp.DataAccess;
+using MovieApp.DataAccess.Implementations;
+using MovieApp.Domain.Models;
+using MovieApp.Services.Implementations;
+using MovieApp.Services.Interfaces;
+
+namespace MovieApp.Helpers
+{
+    public static class DependencyInjectionHelper
+    {
+        public static void InjectDbContext(IServiceCollection services)
+        {
+            services.AddDbContext<MovieAppDbContext>(x =>
+            {
+                x.UseSqlServer("Server=.;Database=MovieAppDb;Trusted_Connection=True;TrustServerCertificate=True;");
+            });
+        }
+
+        public static void InjectRepositorys(IServiceCollection services)
+        {
+            services.AddTransient<IRepository<Movie>, MovieRepository>();
+        }
+
+        public static void InjectServices(IServiceCollection services)
+        {
+            services.AddTransient<IMovieService, MovieService>();
+        }
+    }
+}
