f8ecae1b1ccb32fdb5be0ffbb594127f1322b335
Angel Krstevski <angelkrstevski66@gmail.com>
2025-08-28 03:49:55 +0200

    Add files via upload

---
+++ WorkShop/MovieApp.DataAccess/Implementations/MovieRepository.cs
@@ -0,0 +1,37 @@
+﻿using MovieApp.Domain.Models;
+using System.Diagnostics.CodeAnalysis;
+
+namespace MovieApp.DataAccess.Implementations
+{
+    public class MovieRepository : IRepository<Movie>
+    {
+        private readonly MovieAppDbContext _movieAppDbContext;
+        public MovieRepository(MovieAppDbContext context)
+        {
+            _movieAppDbContext = context;
+        }
+        public List<Movie> GetAll()
+        {
+            return _movieAppDbContext.Movies.ToList();
+        }
+        public Movie GetById(int id)
+        {
+            return _movieAppDbContext.Movies.FirstOrDefault(x => x.Id == id);
+        }
+        public void Add(Movie entity)
+        {
+            _movieAppDbContext.Movies.Add(entity);
+            _movieAppDbContext.SaveChanges();
+        }
+        public void Update(Movie entity)
+        {
+            _movieAppDbContext.Movies.Update(entity);
+            _movieAppDbContext.SaveChanges();
+        }
+        public void Delete(int id)
+        {
+            _movieAppDbContext.Movies.Remove(GetById(id));
+            _movieAppDbContext.SaveChanges();
+        }
+    }
+}
---
+++ WorkShop/MovieApp.Domain/Enums/Genre.cs
@@ -0,0 +1,16 @@
+﻿namespace MovieApp.Domain.Enums
+{
+    public enum Genre
+    {
+        Action = 1,
+        Comedy,
+        Drama = 3,
+        Horror,
+        SciFi = 5,
+        Romance,
+        Thriller = 7,
+        Documentary,
+        Animation = 9,
+        Fantasy
+    }
+}
