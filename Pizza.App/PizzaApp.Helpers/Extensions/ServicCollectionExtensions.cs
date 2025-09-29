be1662d1bc6ace938a46136179947513fe74ed78
Angel Krstevski <angelkrstevski66@gmail.com>
2025-09-29 20:36:01 +0200

    Add files via upload

---
+++ Pizza.App/PizzaApp.Helpers/Extensions/ServicCollectionExtensions.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using PizzaApp.DataAccess.DbContext;
+using PizzaApp.Domain.Entities;
+using Swashbuckle.AspNetCore.Filters;
+using System.Text;
+
+namespace PizzaApp.Helpers.Extensions
+{
+    public static class ServicCollectionExtensions
+    {
+        public class ConfigBuilder
+        {
+            public IServiceCollection Services { get; set; }
+            public IConfiguration Configuration { get; set; }
+            public IdentityBuilder Identity { get; set; }
+            public AuthenticationBuilder AuthenticationBuilder { get; set; }
+
+            public ConfigBuilder(IServiceCollection services, IConfiguration configuration)
+            {
+                Services = services;
+                Configuration = configuration;
+            }
+
+        }
+
+        public static ConfigBuilder AddPostgreSqlDbContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetSection("ConnectionsString").Value;
+            services.AddDbContext<PizzaAppDbContext>(options =>
+            options.UseNpgsql(connectionString));
+
+            return new(services, configuration);
+        }
+        public static ConfigBuilder AddSwager(this ConfigBuilder builder)
+        {
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme // using microsoft.openapi.models
+                {
+                    Description = "Standard Authorization header using the beared scheme, e.g." +
+                    "\bearer {token} \"",
+                    In = ParameterLocation.Header,
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey
+                });
+                c.OperationFilter<SecurityRequirementsOperationFilter>(); // install swashbucke.aspnetcore.filters
+            });
+
+            return builder;
+        }
+        public static ConfigBuilder AddIdentity(this ConfigBuilder builder)
+        {
+            builder.Services.AddIdentityCore<User>(option =>
+            {
+                option.SignIn.RequireConfirmedAccount = false;
+            })
+                .AddEntityFrameworkStores<PizzaAppDbContext>()
+                .AddDefaultTokenProviders();
+
+            return builder;
+        }
+        public static ConfigBuilder AddCors(this ConfigBuilder builder)
+        {
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("CORSPolicy", builder => builder
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowCredentials()
+                .SetIsOriginAllowed((hosts) => true));
+            });
+
+            return builder;
+        }
+        public static ConfigBuilder AddAuthentication(this ConfigBuilder builder)
+        {
+            builder.AuthenticationBuilder = builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            });
+
+            return builder;
+        }
+        public static ConfigBuilder AddJwt(this ConfigBuilder configBuilder, IConfiguration configuration)
+        {
+            var token = configuration.GetSection("Token").Value;
+            configBuilder.AuthenticationBuilder.AddJwtBearer(options =>
+            {
+                options.SaveToken = true;
+                options.RequireHttpsMetadata = true;
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(token)),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                };
+            });
+
+            return configBuilder;
+        }
+    }
+}
